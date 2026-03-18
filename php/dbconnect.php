eb342b8183202db83c7df1dfbcab187c6df76b7b
Keian Gacillos <gacilloskeian02@gmail.com>
2026-03-19 06:13:41 +0800

    I just added the jquery js

---
+++ php/dbconnect.php
@@ -0,0 +1,23 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "mamshernani";
+
+    try {
+
+        $conn = new PDO(
+            "mysql:host=$host;dbname=$dbname",$user,$pass
+        );
+
+
+        $conn ->setAttribute(PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    } catch (PDOException $e) {
+        echo "Connection failed " . $e->getMessage();
+    }
+
+    ?>
